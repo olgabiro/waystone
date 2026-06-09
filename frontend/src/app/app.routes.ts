517bb8e044a123b486ec37096548da0d4c899ac4
Olga Biro <ola@biro.family>
2026-06-09 11:28:37 +0200

    Added routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Task } from './task/task';
+import { Note } from './note/note';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Task,
+  },
+  {
+    path: 'notes/:id',
+    component: Note,
+  },
+];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Task } from './task/task';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Task],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
